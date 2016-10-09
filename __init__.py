fdf3de368f3744ae40fd08e141ab62ee1fa1d250
root <root@vps-259520-4624.hosted.at.hostnet.nl>
2016-10-09 19:38:27 +0200

    Added mollie payment

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect
 from pg_api import Database as pg
 from forms import *
 import sys
+import	Mollie
+from mollie_api_key import *
 
 app = Flask(__name__)
 
@@ -18,6 +20,16 @@ def books_page():
 def portfolio_page():
     return render_template("portfolio.html")
 
+@app.route("/paysuccess/")
+def paysucces():
+    return render_template("paysucces.html")
+
+@app.route("/payfail/")
+def payfail():
+    return render_template("payfail.html")
+
+
+
 @app.route("/order_book/", methods=["GET","POST"])
 def order_book():
     form = OrderForm()
@@ -32,8 +44,23 @@ def order_book():
                 "klanten",
                 "('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')".format(naam, email, telefoon, straat, postcode, code)
         )
-        return redirect("/")
+        return pay() 
     return render_template("tinka.html", form=form)
+
+def pay():
+    mollie = Mollie.API.Client()
+    mollie.setApiKey(api_key)
+
+    try:
+        payment = mollie.payments.create({
+	    "amount": 17.90,
+	    "description":"Voorhetverhaal",
+	    "redirectUrl": "http://voorhetverhaal.nl/paysuccess",
+	})
+	return redirect(payment.getPaymentUrl())
+    except Mollie.API.Error as e:
+	return render_template("payfail.html")
+
 
 
 
